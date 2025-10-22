f4665e9ffdd00b1ebe18b42ca8244b912ff9e7d4
devluizfillipe-eds <devluizfillipe@gmail.com>
2025-10-22 18:59:36 -0300

    feat: atualizações pós erro de deploy no vercel

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: undefined, // Desativa Turbopack
+  },
+}
+
+module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
-// tailwind.config.js
+
 module.exports = {
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
